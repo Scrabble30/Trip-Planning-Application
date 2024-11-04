1aca8cd2adf23bfb9746c4644f8a19162d3a75be
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-11-04 11:16:50 +0100

    Test endpoints

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
+import app.config.AppConfig;
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("trip_planning_db");
+
+        AppConfig.startServer(7070, emf);
     }
 }
